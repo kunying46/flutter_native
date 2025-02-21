2a85bcf4eb7485ace434a5a1f541664a35447071
Nicharat Ketprathum <yingzaza5555@gmail.com>
2025-02-21 09:52:45 +0700

    Inital commit

---
+++ lib/screens/places.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project2/providas/user_places.dart';
+import 'package:project2/screens/add_place.dart';
+import 'package:project2/widgets/places_list.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: userPlaces,
+      ),
+    );
+  }
+}
